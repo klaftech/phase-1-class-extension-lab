872cf58d2d9183f7b1122ea63fc27ea437eed621
Elchonon <ekn400@gmail.com>
2024-11-13 15:49:03 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Polygon {
+    constructor(arrInt){
+        this.sides = arrInt
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        return this.sides.reduce((acc, cur) => acc + cur)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        if(this.sides[0] + this.sides[1] > this.sides[2]){
+            if(this.sides[1] + this.sides[2] > this.sides[0]){
+                if(this.sides[2] + this.sides[0] > this.sides[1]){
+                    return true
+                }
+            } 
+        }
+        return false
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        if((this.sides[0] == this.sides[1]) && (this.sides[1] == this.sides[2]) && (this.sides[2] == this.sides[3])){
+            return true
+        } else {
+            return false
+        } 
+    }
+
+    get area(){
+        return this.sides[0] * this.sides[0]
+    }
+}
